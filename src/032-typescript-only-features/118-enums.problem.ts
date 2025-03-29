37d6ab7ede02ad5d591732003d97ae31fb74a1a7
aloundoye <aloutndoye.an@gmail.com>
2025-03-29 14:09:09 +0000

    typescript only feature

--- src/032-typescript-only-features/118-enums.problem.ts
+++ src/032-typescript-only-features/118-enums.problem.ts
@@ -1,56 +1,60 @@
-import { expect, it, vi } from "vitest";
+import { expect, it, vi } from 'vitest';
 
-const LogLevel = {
-  DEBUG: 0,
-  INFO: 1,
-  WARN: 2,
-  ERROR: 3,
-};
+enum LogLevel {
+  DEBUG,
+  INFO,
+  WARN,
+  ERROR,
+}
 
-function log(opts: { globalLogLevel: number; level: number; message: string }) {
+function log(opts: {
+  globalLogLevel: LogLevel;
+  level: LogLevel;
+  message: string;
+}) {
   if (opts.level >= opts.globalLogLevel) {
     console.log(opts.message);
   }
 }
 
-it("Should log if the level is higher than the global log level", () => {
-  const consoleLog = vi.spyOn(console, "log");
+it('Should log if the level is higher than the global log level', () => {
+  const consoleLog = vi.spyOn(console, 'log');
   log({
     globalLogLevel: LogLevel.INFO,
     level: LogLevel.ERROR,
-    message: "Hello!",
+    message: 'Hello!',
   });
 
-  expect(consoleLog).toHaveBeenCalledWith("Hello!");
+  expect(consoleLog).toHaveBeenCalledWith('Hello!');
 });
 
-it("Should log if the level is equal to the global log level", () => {
-  const consoleLog = vi.spyOn(console, "log");
+it('Should log if the level is equal to the global log level', () => {
+  const consoleLog = vi.spyOn(console, 'log');
   log({
     globalLogLevel: LogLevel.INFO,
     level: LogLevel.INFO,
-    message: "Hello!",
+    message: 'Hello!',
   });
 
-  expect(consoleLog).toHaveBeenCalledWith("Hello!");
+  expect(consoleLog).toHaveBeenCalledWith('Hello!');
 });
 
-it("Should not log if the level is lower than the global log level", () => {
-  const consoleLog = vi.spyOn(console, "log");
+it('Should not log if the level is lower than the global log level', () => {
+  const consoleLog = vi.spyOn(console, 'log');
   log({
     globalLogLevel: LogLevel.INFO,
     level: LogLevel.DEBUG,
-    message: "Hello!",
+    message: 'Hello!',
   });
 
   expect(consoleLog).not.toHaveBeenCalled();
 });
 
-it("Should give you a TS error if you pass an invalid log level", () => {
+it('Should give you a TS error if you pass an invalid log level', () => {
   log({
     globalLogLevel: LogLevel.INFO,
     // @ts-expect-error
     level: 123,
-    message: "Hello!",
+    message: 'Hello!',
   });
 });
